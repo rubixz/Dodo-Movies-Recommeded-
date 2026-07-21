5b80d8f7a11c1daba8aca69171e44c03558bcb72
Rubiya Khan <rubikhan1411@gmail.com>
2026-07-21 13:15:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,20 +54,20 @@ if st.button('Show Recommendation'):
         st.text(recommended_movie_names[4])
         st.image(recommended_movie_posters[4])
     with col6:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
+        st.text(recommended_movie_names[5])
+        st.image(recommended_movie_posters[5])
     with col7:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
+        st.text(recommended_movie_names[6])
+        st.image(recommended_movie_posters[6])
     with col8:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
+        st.text(recommended_movie_names[7])
+        st.image(recommended_movie_posters[7])
     with col9:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
+        st.text(recommended_movie_names[8])
+        st.image(recommended_movie_posters[8])
     with col10:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
+        st.text(recommended_movie_names[9])
+        st.image(recommended_movie_posters[9])
 
 
 
